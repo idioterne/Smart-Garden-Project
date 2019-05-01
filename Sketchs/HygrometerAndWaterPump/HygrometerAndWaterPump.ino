1808335e46dc05cddf7ca4ecb00e8a1571e232df
Stephan D <37081797+Stephan-MDD@users.noreply.github.com>
2019-05-01 12:09:16 +0200

    changed variable name and added serialprint

--- Sketchs/HygrometerAndWaterPump/HygrometerAndWaterPump.ino
+++ Sketchs/HygrometerAndWaterPump/HygrometerAndWaterPump.ino
@@ -2,7 +2,7 @@ const int HYGROMETER_PIN = A0;
 const int PUMP_PIN = 3;
 
 int hygroValue;
-int motorSpeed;
+int pumpSpeed;
 
 void setup() {
   Serial.begin(9600);
@@ -11,8 +11,8 @@ void setup() {
 
 void loop() {
   hygroValue = analogRead(HYGROMETER_PIN);
-  motorSpeed = (hygroValue < 800) ? 255 : 0;
-  analogWrite(PUMP_PIN, motorSpeed);
-  
+  pumpSpeed = (hygroValue < 800) ? 255 : 0;
+  analogWrite(PUMP_PIN, pumpSpeed);
+  Serial.println(hygroValue);
   delay(2000);
 }
