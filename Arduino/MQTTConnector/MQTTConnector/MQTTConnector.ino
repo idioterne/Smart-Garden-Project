8dd10bee3ad04740778073965753dacb85ba88b3
Stephan D <stephan-mdd@gmail.com>
2019-05-20 13:09:09 +0200

    MQTT Connector

---
+++ Arduino/MQTTConnector/MQTTConnector/MQTTConnector.ino
@@ -0,0 +1,17 @@
+#include "MQTTConnector.h"
+
+char* ssid = "CPHBUS-STUDENT";
+char* ssPassword = "";
+char* mqttUser = "Device";
+char* mqttPassword = "device";
+char* mqttTopic = "landscape";
+
+MQTTConnector mqttConnector(ssid, ssPassword, mqttUser, mqttPassword, mqttTopic);
+
+void setup() {
+   mqttConnector.setup();
+}
+
+void loop() {
+  //mqttConnector.run();
+}
